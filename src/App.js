5d336915ee2db8bc553aff8f97fa28b3d68b4735
DeepEnd007 <yihuihu56@gmail.com>
2024-04-19 12:17:13 +1200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function App() {
+  const url = "https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cf2e2f9556msh6dd4412de3ea5d9p196d2fjsn90b263db04f6",
+      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return <div className="App"></div>;
 }
 
 export default App;
